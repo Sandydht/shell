d9b62b42a2b4e136d5dfe990ea179c72dbfd50b9
sandy <sandydwi75@gmail.com>
2025-05-25 20:43:58 +0700

    feat: finish slicing home page

--- src/app/modules/home/components/app-bar/app-bar.component.ts
+++ src/app/modules/home/components/app-bar/app-bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ProfilePopoverComponent } from './components/profile-popover/profile-popover.component';
+import { NotificationPopoverComponent } from './components/notification-popover/notification-popover.component';
 
 @Component({
   selector: 'app-app-bar',
-  imports: [],
+  imports: [
+    ProfilePopoverComponent,
+    NotificationPopoverComponent
+  ],
   templateUrl: './app-bar.component.html',
   styleUrl: './app-bar.component.css'
 })
